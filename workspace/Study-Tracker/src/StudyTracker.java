d05ed6c0d96f8d8d6b1bfe973cb1d37ce015d3a2
bng919 <b.graham@queensu.ca>
2020-11-11 23:36:10 -0500

    Early dev of GUI and timing systems

--- workspace/Study-Tracker/src/StudyTracker.java
+++ workspace/Study-Tracker/src/StudyTracker.java
@@ -1,9 +1,61 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class StudyTracker {
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+public class StudyTracker implements ActionListener{
+
+	//Initialize global variables
+	private JFrame frame;
+	private JButton button;
+	private boolean active = false;
+	private long startTime;
+	private long elapsedTime;
+	private Proj task = new Proj("Study");
+	
+	public StudyTracker() {
+		frame = new JFrame();
+		
+		button = new JButton("Start");
+		button.addActionListener(this);
+
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Study Tracker");
+		frame.setSize(200, 200);
+		frame.setVisible(true);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		new StudyTracker();
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if(!active) {
+			startTime = System.currentTimeMillis();
+			active = true;
+			button.setText("Stop");
+		} else {
+			elapsedTime = System.currentTimeMillis() - startTime;
+			active = false;
+			task.updateTime(elapsedTime);
+			System.out.println(task.getTime());
+			button.setText("Start");
+		}
+		
 	}
 
 }
